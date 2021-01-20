18e76ffcf3605265e9094d7c8920db7c9c3e7fc4
y20107 <yumoto20107@gmail.com>
2021-01-20 15:51:37 +0900

    Userモデル単体テスト作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 3)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
+    password_confirmation {password}
+    last_name             {"山田"}
+    first_name            {"隆太郎"}
+    last_name_kana        {"ヤマダ"}
+    first_name_kana       {"リクタロウ"}
+    birth_date            {"Faker::Date.between(from: '1930-01-01', to: '2016-12-31')"}
+  end
+end
