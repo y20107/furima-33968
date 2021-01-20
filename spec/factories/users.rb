24d8bfb7a15c3c2590bc42647d509c56d7a9a6e4
y20107 <yumoto20107@gmail.com>
2021-01-20 18:52:01 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     first_name            {"隆太郎"}
     last_name_kana        {"ヤマダ"}
     first_name_kana       {"リクタロウ"}
-    birth_date            {"Faker::Date.between(from: '1930-01-01', to: '2016-12-31')"}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
   end
 end
