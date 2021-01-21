97c934a8e9be4202266570474b4ce1ad69e2c4c8
y20107 <yumoto20107@gmail.com>
2021-01-21 17:54:28 +0900

    createアクション作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
@@ -7,11 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:title, :item_description, :category_id, :item_state_id, :shipping_cost_id, :shipping_area_id, :shipping_time_id, :cost, :image)
+    params.require(:item).permit(:title, :item_description, :category_id, :item_state_id, :shipping_cost_id, :shipping_area_id, :shipping_time_id, :cost, :image).merge(user_id: current_user)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,23 @@ class Item < ApplicationRecord
   belongs_to :shipping_time
 
   has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :item_description
+    validates :category_id
+    validates :item_state_id
+    validates :shipping_cost_id
+    validates :shipping_area_id
+    validates :shipping_time_id
+    validates :cost,
+              format: { 
+                with: /\A[\d]+\z/, message: 'Include both letters and numbers' 
+              },
+              numericality: {
+                greater_than_or_equal_to: 300,
+                less_than_or_equal_to: 9999999
+              }
+  end
 end
